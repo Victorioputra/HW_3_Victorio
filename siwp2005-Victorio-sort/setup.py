2319631c3d6a4a8fc2600c19a4c018af76bc7dbb
nedd18 <benedbened18@gmail.com.>
2024-06-15 23:12:25 +0700

    HW#

--- siwp2005-Victorio-sort/setup.py
+++ siwp2005-Victorio-sort/setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='siwp2005-Victorio-sort',
-    version='0.1.0',
+    version='0.1.2',
     packages=find_packages(where='src'),
-    package_dir={'': 'src'},
+    long_description = open('README.md').read(),
+    long_description_content_type = 'text/markdown',
     install_requires=[],
     test_suite='tests',
     tests_require=['unittest'],
